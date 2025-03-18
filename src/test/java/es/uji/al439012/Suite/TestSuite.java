d583463698a3b1199c6b0d0d7809ee682e2b9c9e
al439012 <al439012@uji.es>
2025-03-18 10:56:17 +0100

    akdrjh

---
+++ src/test/java/es/uji/al439012/Suite/TestSuite.java
@@ -0,0 +1,18 @@
+package es.uji.al439012.Suite;
+
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Lanzar todos los tests")
+@SelectPackages({
+        "es.uji.al439012.csv",
+        "es.uji.al439012.KNN",
+        "es.uji.al439012.table"
+})
+@IncludeClassNamePatterns(".*Test")
+public class TestSuite {
+}
+
---
+++ src/test/java/es/uji/al439012/main/MainTest.java
@@ -0,0 +1,7 @@
+package es.uji.al439012.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
