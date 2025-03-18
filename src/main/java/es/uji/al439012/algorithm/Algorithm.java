835d531019c6591702f62b0987f5e012400b809c
al439012 <al439012@uji.es>
2025-03-18 11:37:23 +0100

    porfa funciona

--- src/main/java/es/uji/al439012/algorithm/Algorithm.java
+++ src/main/java/es/uji/al439012/algorithm/Algorithm.java
@@ -4,5 +4,6 @@ import java.util.List;
 
 public interface Algorithm<T> {
     void train(T data) throws Exception;
-    Integer estimate(List<Double> dato);
+    Integer estimate(List<Double> data
+    );
 }
